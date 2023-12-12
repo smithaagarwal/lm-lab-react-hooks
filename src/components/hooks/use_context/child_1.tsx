0a2ed92628242b0b85f337e56649a64673860ec7
smithaagarwal <smitha.kotian@gmail.com>
2023-12-12 20:37:54 +0000

    feat: add logic to use useEffect hook

--- src/components/hooks/use_context/child_1.tsx
+++ src/components/hooks/use_context/child_1.tsx
@@ -2,6 +2,6 @@ import { useContext } from "react";
 import { ThemeContext } from "./parent";
 
 export const Child1: React.FC = () => {
-	const theme = useContext(ThemeContext);
-	return <div style={theme}>Child 1</div>;
+  const theme = useContext(ThemeContext);
+  return <div style={theme}>Child 1</div>;
 };
--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
+
 // make an api call using useEffect
+interface ToDo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
 export const APICall = () => {
+  const [jsonData, setJsonData] = useState<ToDo | null>(null);
 
-    // Your code here!
+  useEffect(() => {
+    const fetchToDo = async () => {
+      try {
+        const response = await fetch(jsonTypicode);
+        const json = await response.json();
+        setJsonData(json);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+    fetchToDo();
+  }, []);
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+  return (
+    <>
+      <h2>useEffect</h2>
+      <p>{jsonData ? jsonData.title : "Fetching data..."}</p>
+    </>
+  );
+};
