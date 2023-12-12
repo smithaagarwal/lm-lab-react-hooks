d58a070ca0ffc22c8828f40b40e34b3d656df1fb
smithaagarwal <smitha.kotian@gmail.com>
2023-12-12 12:58:20 +0000

    feat: add logic to use useContext hook

--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -1,22 +1,42 @@
-import { useState } from 'react';
-import { Child1 } from './child_1';
-import { Child2 } from './child_2';
+import { useState, createContext } from "react";
+import { Child1 } from "./child_1";
+import { Child2 } from "./child_2";
+
+interface Theme {
+  backgroundColor: string;
+  color: string;
+  padding: string;
+  margin: string;
+}
+export const ThemeContext = createContext<Theme>({
+  backgroundColor: "",
+  color: "",
+  padding: "",
+  margin: "",
+});
 
 export const Parent = () => {
+  const [darkTheme, setDarkTheme] = useState(true);
 
-    const [darkTheme, setDarkTheme] = useState(true)
+  const toggleTheme = () => {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  };
 
-    const toggleTheme = () => {
-        setDarkTheme(prevDarkTheme => !prevDarkTheme)
-    }
+  const theme = {
+    backgroundColor: darkTheme ? "#333" : "#CCC",
+    color: darkTheme ? "white" : "#333",
+    padding: "2rem",
+    margin: "2rem",
+  };
 
-    return (
-        <div className='section'>
-            <h2>useContext</h2>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-            <Child1 dark={darkTheme} />
-            <Child2 dark={darkTheme} />
-        </div>
-
-    )
-}
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div className="section">
+        <h2>useContext</h2>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <Child1 />
+        <Child2 />
+      </div>
+    </ThemeContext.Provider>
+  );
+};
