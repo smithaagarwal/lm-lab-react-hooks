946ac3c72ef6a78fba7fb2a8864ffd97ec77f22d
smithaagarwal <smitha.kotian@gmail.com>
2023-12-12 21:13:45 +0000

    feat: add logic to use useMemo hook

--- src/components/hooks/use_memo/useMemo.tsx
+++ src/components/hooks/use_memo/useMemo.tsx
@@ -1,30 +1,31 @@
-import { useState } from 'react';
+import { useState, useMemo } from "react";
 
 export const MemoExample = () => {
+  console.log("Rendering component...");
 
-	console.log("Rendering component...")
+  const [numberObj, setNumberObj] = useState({ input: 0 });
 
-	const [numberObj, setNumberObj] = useState({ input: 0 });
-	
-	const doubleNumber = slowFunction(numberObj.input)
+  const doubleNumber = useMemo(() => {
+    return slowFunction(numberObj.input);
+  }, [numberObj.input]);
 
-	return (
-		<>
-			<h2>useMemo</h2>
+  return (
+    <>
+      <h2>useMemo</h2>
 
-			<button onClick={() => setNumberObj({ input: 3 })}>Double 3</button>
+      <button onClick={() => setNumberObj({ input: 3 })}>Double 3</button>
 
-			<p className='use-memo__text'>{doubleNumber}</p>
-		</>
-	);
+      <p className="use-memo__text">{doubleNumber}</p>
+    </>
+  );
 };
 
 function slowFunction(num: number) {
-	console.log('calling slow function... 🐌');
-	for (let i = 0; i <= 1000000000; i++) {
-		// ⏰
-	}
-	const result = num * 2;
-	console.log(`result: ${result}`);
-	return result;
+  console.log("calling slow function... 🐌");
+  for (let i = 0; i <= 1000000000; i++) {
+    // ⏰
+  }
+  const result = num * 2;
+  console.log(`result: ${result}`);
+  return result;
 }
